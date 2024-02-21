5829bab84134ab80e5453f18b36a627752ef30a2
Alisha M Irfan <160208713+Alisha000111@users.noreply.github.com>
2024-02-21 16:30:39 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+var members = ["alisha", "muhammad", "irfan", "alisha", "irfan"];
+var massage = "kal party h kis ki:";
+for (var i = 0; i < members.length; i++) {
+    console.log(massage + members[i]);
+}
---
+++ app.ts
@@ -0,0 +1,5 @@
+let members: string[] =[`alisha`,`muhammad`,`irfan`,`alisha`,`irfan`];
+let massage: string=`kal party h kis ki:`;
+for(let i=0; i<members.length; i++){
+console.log(massage + members[i]);
+}
